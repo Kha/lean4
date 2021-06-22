44f0bc34773c8b269b29ffa871b0844a2d87b497
Sebastian Ullrich <sebasti@nullri.ch>
2021-06-22 22:00:11 +0200

    chore: sanitized build with stack traces

--- shell.nix
+++ shell.nix
@@ -1,15 +1,20 @@
 let
   flakePkgs = (import ./default.nix).packages.${builtins.currentSystem};
-in { pkgs ? flakePkgs.nixpkgs, llvmPackages ? null }:
+in { pkgs ? flakePkgs.nixpkgs, llvmPackagesName ? null }:
+let llvmPackages = if llvmPackagesName == null
+                   then flakePkgs.llvmPackages
+                   else pkgs.${"llvmPackages_${llvmPackagesName}"}; in
 # use `shell` as default
 (attribs: attribs.shell // attribs) rec {
   inherit (flakePkgs) temci;
   shell = pkgs.mkShell.override {
-    stdenv = pkgs.overrideCC pkgs.stdenv (if llvmPackages == null
-                                          then flakePkgs.llvmPackages
-                                          else pkgs.${"llvmPackages_${llvmPackages}"}).clang;
+    stdenv = pkgs.overrideCC pkgs.stdenv llvmPackages.clang;
   } rec {
-    buildInputs = with pkgs; [ cmake (gmp.override { withStatic = true; }) ccache temci ];
+    buildInputs = with pkgs; [
+      cmake (gmp.override { withStatic = true; }) ccache temci
+      # for llvm-symbolizer
+      llvmPackages.bintools
+    ];
     # https://github.com/NixOS/nixpkgs/issues/60919
     hardeningDisable = [ "all" ];
     # more convenient `ctest` output
