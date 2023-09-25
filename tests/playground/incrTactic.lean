2d1436989b9d64b705776a6d378e018678a189b2
Sebastian Ullrich <sebasti@nullri.ch>
2023-09-25 13:25:06 +0200

    progress

--- tests/playground/incrTactic.lean
+++ tests/playground/incrTactic.lean
@@ -28,6 +28,7 @@ opaque Fix.unmk [∀ α, Nonempty (F α)] : Fix F → F (Fix F)
 
 open Lean Server Elab Command
 
+/-
 /-- Result of processing a snapshot -/
 inductive SnapshotF.Result (Snapshot : Type)
   /-- Forces the current file worker to restart; used when the set of imports has changed. -/
@@ -58,89 +59,128 @@ def Snapshot.ofProcess
 
 
   }
+-/
 
-def withFatalExceptions (act : IO Snapshot.Result) : BaseIO Snapshot.Result := do
+structure Snapshot where
+  msgLog : MessageLog
+  stx : Syntax
+
+abbrev SnapshotTask α := Task α
+
+structure TacticSnapshotF (TacticSnapshot : Type) extends Snapshot where
+  next? : Option (SnapshotTask TacticSnapshot)
+abbrev TacticSnapshot := Fix TacticSnapshotF
+
+structure CommandSnapshotF (CommandParseSnapshot : Type) extends Snapshot where
+  nextTac? : Option (SnapshotTask TacticSnapshot)
+  next? : Option (SnapshotTask CommandParseSnapshot)
+
+structure CommandParseSnapshotF (CommandParseSnapshot : Type) extends Snapshot where
+  next? : Option (SnapshotTask (CommandSnapshotF CommandParseSnapshot))
+
+abbrev CommandParseSnapshot := Fix CommandParseSnapshotF
+abbrev CommandSnapshot := CommandSnapshotF CommandParseSnapshot
+
+structure HeaderSnapshot extends Snapshot where
+  cmdState : Command.State
+  next? : Option (SnapshotTask CommandSnapshot)
+
+structure HeaderParseSnapshot extends Snapshot where
+  next? : Option (SnapshotTask HeaderSnapshot)
+
+abbrev InitialSnapshot := HeaderParseSnapshot
+
+def withFatalExceptions (ex : Snapshot → α) (act : IO α) : BaseIO α := do
   match (← act.toBaseIO) with
-  | .error e => return .next <| .mk {
-    messages := MessageLog.empty.add { fileName := "TODO", pos := ⟨0, 0⟩, data := e.toString }
-    next := .pure .eof
+  | .error e => return ex {
+    stx := .missing
+    msgLog := MessageLog.empty.add { fileName := "TODO", pos := ⟨0, 0⟩, data := e.toString }
   }
   | .ok a => return a
 
+def getOrCancel (t? : Option (SnapshotTask α)) : BaseIO (Option α) := do
+  let some t := t? | return none
+  if (← IO.hasFinished t) then
+    return t.get
+  IO.cancel t
+  return none
+
 open Lean.Server.FileWorker in
-partial def processLean (hOut : IO.FS.Stream) (opts : Options) (doc : DocumentMeta) : BaseIO Snapshot :=
-  initial doc
+partial def processLean (hOut : IO.FS.Stream) (opts : Options) (doc : DocumentMeta) (old? : Option InitialSnapshot) :
+    BaseIO InitialSnapshot :=
+  parseHeader old?
 where
-  initial (doc : DocumentMeta) : BaseIO Snapshot := return .mk {
-    messages := .empty
-    next := ← BaseIO.asTask (parseHeader none doc)
-    reprocess := fun doc => do BaseIO.asTask <| return .next (← initial doc)
-  }
-  parseHeader (oldStx : Option Syntax) (m : DocumentMeta) := Snapshot.ofProcess <| withFatalExceptions do
-    let (headerStx, headerParserState, msgLog) ← do
-      Parser.parseHeader m.mkInputContext
-    return .next <| .mk {
-      messages := msgLog
-      reprocess := parseHeader headerStx m
-      next := fun m => withFatalExceptions (beginPos := 0) do
-        -- COPIED
-        let mut srcSearchPath ← initSrcSearchPath (← getBuildDir)
-        let lakePath ← match (← IO.getEnv "LAKE") with
-          | some path => pure <| System.FilePath.mk path
-          | none =>
-            let lakePath ← match (← IO.getEnv "LEAN_SYSROOT") with
-              | some path => pure <| System.FilePath.mk path / "bin" / "lake"
-              | _         => pure <| (← IO.appDir) / "lake"
-            pure <| lakePath.withExtension System.FilePath.exeExtension
-        let (headerEnv, msgLog) ← try
-          if let some path := System.Uri.fileUriToPath? m.uri then
-            -- NOTE: we assume for now that `lakefile.lean` does not have any non-stdlib deps
-            -- NOTE: lake does not exist in stage 0 (yet?)
-            if path.fileName != "lakefile.lean" && (← System.FilePath.pathExists lakePath) then
-              let pkgSearchPath ← lakeSetupSearchPath lakePath m (Lean.Elab.headerToImports headerStx) hOut
-              srcSearchPath ← initSrcSearchPath (← getBuildDir) pkgSearchPath
-          Elab.processHeader headerStx opts .empty m.mkInputContext
-        catch e =>  -- should be from `lake print-paths`
-          let msgs := MessageLog.empty.add { fileName := "<ignored>", pos := ⟨0, 0⟩, data := e.toString }
-          pure (← mkEmptyEnvironment, msgs)
-        let mut headerEnv := headerEnv
-        try
-          if let some path := System.Uri.fileUriToPath? m.uri then
-            headerEnv := headerEnv.setMainModule (← moduleNameOfFileName path none)
-        catch _ => pure ()
-        let cmdState := Elab.Command.mkState headerEnv msgLog opts
-        let cmdState := { cmdState with infoState := {
-          enabled := true
-          trees := #[Elab.InfoTree.context ({
-            env     := headerEnv
-            fileMap := m.text
-            ngen    := { namePrefix := `_worker }
-          }) (Elab.InfoTree.node
-              (Elab.Info.ofCommandInfo { elaborator := `header, stx := headerStx })
-              (headerStx[1].getArgs.toList.map (fun importStx =>
-                Elab.InfoTree.node (Elab.Info.ofCommandInfo {
-                  elaborator := `import
-                  stx := importStx
-                }) #[].toPArray'
-              )).toPArray'
-          )].toPArray'
-        }}
-        -- END COPIED
-        return .next <| .mk {
-          beginPos := 0
-          messages := cmdState.messages
-          processNext := processCmd none headerParserState cmdState
-        }
-  }
-  processCmd (oldStx : Option Syntax) (mpState : Parser.ModuleParserState) (cmdState : Command.State) := fun doc => do
+  parseHeader (old? : Option HeaderParseSnapshot) := withFatalExceptions ({ · with stx := .missing, next? := none }) do
+    let (stx, parserState, msgLog) ← do
+      Parser.parseHeader doc.mkInputContext
+    let oldNext? ← old?.bind (·.next?) |> getOrCancel
+    return { stx, msgLog, next? := some (← BaseIO.asTask <| processHeader oldNext? stx parserState) }
+  processHeader (old? : Option HeaderSnapshot) (stx : Syntax) (parserState : Parser.ModuleParserState) := withFatalExceptions ({ · with next? := none }) do
+    if let some old := old? then
+      if old.stx == stx then
+        let oldNext? ← old?.bind (·.next?) |> getOrCancel
+        return { old with next? := some (← BaseIO.asTask <| parseCmd oldNext? parserState old.cmdState) }
+    -- COPIED
+    let mut srcSearchPath ← initSrcSearchPath (← getBuildDir)
+    let lakePath ← match (← IO.getEnv "LAKE") with
+      | some path => pure <| System.FilePath.mk path
+      | none =>
+        let lakePath ← match (← IO.getEnv "LEAN_SYSROOT") with
+          | some path => pure <| System.FilePath.mk path / "bin" / "lake"
+          | _         => pure <| (← IO.appDir) / "lake"
+        pure <| lakePath.withExtension System.FilePath.exeExtension
+    let (headerEnv, msgLog) ← try
+      if let some path := System.Uri.fileUriToPath? doc.uri then
+        -- NOTE: we assume for now that `lakefile.lean` does not have any non-stdlib deps
+        -- NOTE: lake does not exist in stage 0 (yet?)
+        if path.fileName != "lakefile.lean" && (← System.FilePath.pathExists lakePath) then
+          let pkgSearchPath ← lakeSetupSearchPath lakePath doc (Lean.Elab.headerToImports stx) hOut
+          srcSearchPath ← initSrcSearchPath (← getBuildDir) pkgSearchPath
+      Elab.processHeader stx opts .empty doc.mkInputContext
+    catch e =>  -- should be from `lake print-paths`
+      let msgs := MessageLog.empty.add { fileName := "<ignored>", pos := ⟨0, 0⟩, data := e.toString }
+      pure (← mkEmptyEnvironment, msgs)
+    let mut headerEnv := headerEnv
+    try
+      if let some path := System.Uri.fileUriToPath? doc.uri then
+        headerEnv := headerEnv.setMainModule (← moduleNameOfFileName path none)
+    catch _ => pure ()
+    let cmdState := Elab.Command.mkState headerEnv msgLog opts
+    let cmdState := { cmdState with infoState := {
+      enabled := true
+      trees := #[Elab.InfoTree.context ({
+        env     := headerEnv
+        fileMap := doc.text
+        ngen    := { namePrefix := `_worker }
+      }) (Elab.InfoTree.node
+          (Elab.Info.ofCommandInfo { elaborator := `header, stx })
+          (stx[1].getArgs.toList.map (fun importStx =>
+            Elab.InfoTree.node (Elab.Info.ofCommandInfo {
+              elaborator := `import
+              stx := importStx
+            }) #[].toPArray'
+          )).toPArray'
+      )].toPArray'
+    }}
+    -- END COPIED
+    return {
+      stx
+      cmdState
+      msgLog := cmdState.messages
+      next? := some (← BaseIO.asTask <| parseCmd (old?.map _) parserState cmdState)
+    }
+  parseCmd (old? : Option CommandSnapshot) (mpState : Parser.ModuleParserState) (cmdState : Command.State) := do
     let beginPos := mpState.pos
     let scope := cmdState.scopes.head!
     let inputCtx := doc.mkInputContext
     let pmctx := { env := cmdState.env, options := scope.opts, currNamespace := scope.currNamespace, openDecls := scope.openDecls }
     let (cmdStx, cmdParserState, msgLog) :=
       Parser.parseCommand inputCtx pmctx mpState cmdState.messages
-    if oldStx == some cmdStx then
-      return .unchanged
+    if let some old := old? then
+      if let some next := old.next? then
+        if let some oldProc ← next.getOrCancel then
+          if old.stx == stx then
+            return { old with next? := some (← BaseIO.asTask <| processHeader oldProc stx parserState) }
     return .next <| .mk {
       beginPos
       messages := msgLog
